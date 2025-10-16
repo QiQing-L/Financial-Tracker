3f8ec7ee5c02293fed3848290c84894fb0270e16
Qi Qing <learn.qiqing@gmail.com>
2025-10-16 11:44:27 -0400

    removed .reversedOrder() added .reversed().
    edit Comparator.thenComparing to sort the time of transaction on the same day in ArrayList in ledgerMenu.
    
    * Not tested.

--- src/main/java/com/pluralsight/FinancialTracker.java
+++ src/main/java/com/pluralsight/FinancialTracker.java
@@ -296,9 +296,9 @@ public class FinancialTracker {
        ------------------------------------------------------------------ */
     private static void ledgerMenu(Scanner scanner) {
         transactions.sort(Comparator
-                .comparing(Transaction::getDate, Comparator.reverseOrder())
-                .thenComparing(Transaction::getTime));
-        //transactions.sort(Comparator.comparing(Transaction ::getDate));
+                .comparing(Transaction::getDate)
+                .thenComparing(Transaction::getTime).reversed());
+        //transactions.sort(Comparator.comparing(Transaction ::getDate, Comparator.reverseOrder()));
 
         boolean running = true;
         while (running) {
